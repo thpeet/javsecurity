256d06e69e83552da9191e34be1c41885b45e649
petitti <otitep.th@operamail.com>
2016-12-21 15:08:09 +0100

    rename package

--- src/org/jse/me/MDigest.java
+++ src/org/jse/me/MDigest.java
@@ -1,29 +1,32 @@
-package org.jse.md;
+package org.jse.me;
 
 import java.security.MessageDigest;
 
-public class MessDigest {
+
+public class MDigest {
 
 	public static void main(String[] args)throws Exception {
 
-		if( args.length !=1){
+		if( args.length !=1 ){
 			System.err.println("Usage: java MessDigest text");
 			System.exit(1);
 		}
 
-		byte[] plainText = args[0].getBytes("UTF8");
 
+		byte[] plainText = args[0].getBytes("UTF8");
 
 		// get a message digest object using the MD5 algorithm
 		MessageDigest md = MessageDigest.getInstance("MD5");
 
-		System.out.println("\n" + md.getProvider().getInfo());
+		System.out.println(md.getProvider().getInfo());
 
 		md.update(plainText);
 		System.out.println(" \nDigest: ");
-		System.out.println( new String( md.digest(), "UTF8"));
 
+		String digest= new String( md.digest(), "UTF8");
+		System.out.println( digest );
 
 	}
+
 
 }
